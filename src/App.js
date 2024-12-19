d6b03122383293cc0c6c6b4fe1b90ebebb1bc164
Hrishi93 <sonumahure@gmail.com>
2024-12-19 14:51:18 +0530

    Registration Portal

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import UserForm from './components/UserForm';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Student Registration Portal</h1>
+        <UserForm />
+      </header>
+    </div>
+  );
+}
+
+export default App;
